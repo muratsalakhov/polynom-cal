37017b7baff39c163accf5f2d0e52a8ed7e63680
muratsalakhov <muratsalakhov@gmail.com>
2021-02-27 19:29:42 +0300

    System Tests added

---
+++ UnitTestProject1/SystemTest/SystemTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1.SystemTest
+{
+    //[CodedUITest]
+    [TestClass]
+    public class SystemTest
+    {
+        public SystemTest () {}
+
+        [TestMethod]
+        public void CodedPolynomCalcCheck()
+        {
+            Assert.AreEqual(true, true);
+            /*this.UIMap.openApplication();
+            this.UIMap.typeText();
+            this.UIMap.clickRun();
+            this.UIMap.assertPolynomCalc();
+            this.UIMap.closeApplication();*/
+        }
+    }
+}
